63c3c7773d6fc648f563eab5b845958d5035ba4a
Richard Jamieson <jamieson.rich@gmail.com>
2024-10-29 17:55:28 +0200

    Create a basic program

--- programs/solana_strategy/src/lib.rs
+++ programs/solana_strategy/src/lib.rs
@@ -1,16 +1,28 @@
 use anchor_lang::prelude::*;
 
-declare_id!("25CLGmMNTKy9xKHpREMfuCgUK3p9ZdhPP8ETxFKoMFtE");
+declare_id!("YourProgramIDHere");
 
 #[program]
-pub mod solana_strategy {
+pub mod my_anchor_project {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(ctx: Context<Initialize>, data: u64) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.data = data;
         Ok(())
     }
 }
 
+#[account]
+pub struct BaseAccount {
+    pub data: u64,
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 8)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
